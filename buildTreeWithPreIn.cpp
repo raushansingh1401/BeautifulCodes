8b3efb2eafe54c3177340f42c14b9a43f2797f40
raushansingh1401 <41655081+raushansingh1401@users.noreply.github.com>
2025-01-13 11:46:36 +0530

    Update buildTreeWithPreIn.cpp

--- buildTreeWithPreIn.cpp
+++ buildTreeWithPreIn.cpp
@@ -1,26 +1,23 @@
-TreeNode* buildTreeHelper(vector<int> &preorder, int startPre, int endPre, vector<int> &inorder, int startIn, int endIn, unordered_map<int, int> &inMap){
-
-        if(startIn > endIn || startPre > endPre) return NULL;
-
-        TreeNode* curr = new TreeNode(preorder[startPre]);
-
-        int currNodeIndex = inMap[preorder[startPre]];
-        int totalElemInLeft = currNodeIndex - startIn;
-
-
-        curr->left = buildTreeHelper(preorder, startPre+1, startPre+totalElemInLeft, inorder, startIn, currNodeIndex-1, inMap);
-        curr->right = buildTreeHelper(preorder, startPre+totalElemInLeft+1, endPre, inorder, currNodeIndex+1, endIn, inMap);
-
-        return curr;
-
+class Solution {
+public:
+    TreeNode* buildTreeHelper(vector<int> &preorder, vector<int> &inorder, unordered_map<int, int> &inMap, int startInd, int endInd, int &preInd){
+        if(startInd > endInd) return NULL;
+            
+        TreeNode* newCreatedNode = new TreeNode(preorder[preInd]);
+        int createdNodeInd = inMap[preorder[preInd]];
+        preInd++;
+            
+        newCreatedNode->left = buildTreeHelper(preorder, inorder, inMap, startInd, createdNodeInd - 1, preInd);
+        newCreatedNode->right = buildTreeHelper(preorder, inorder, inMap, createdNodeInd + 1, endInd, preInd);
+        return newCreatedNode;
     }
 
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         unordered_map<int, int> inMap;
-
-        for(int i=0 ;i<inorder.size(); i++){
+        for(int i = 0; i < inorder.size(); i++){
             inMap[inorder[i]] = i;
         }
-
-        return buildTreeHelper(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1, inMap);
+        int startInd(0), endInd(preorder.size() - 1), preInd(0);
+        return buildTreeHelper(preorder, inorder, inMap, startInd, endInd, preInd);
     }
+};
